42cf6af287bfdac0d9763e8ea5195a019046e275
onkar-midhunter <khedekar679@gmai.com>
2025-11-29 15:30:42 +0530

    add @vercel/analytics integration to layout and package files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { Analytics } from '@vercel/analytics/next';
 import AuthProvider from './context/AuthProvider';
 import { Toaster } from '@/components/ui/toaster';
 
@@ -22,6 +23,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <AuthProvider>
           {children}
           <Toaster />
+          <Analytics/>
         </AuthProvider>
       </body>
     </html>
